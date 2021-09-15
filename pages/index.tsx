3444ec82cc8451ec1d1a4ff9a7ce3bcccffa57a1
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-15 20:45:43 +0900

    inputにsetFocusを当てるのと、modalを新しく実装；。

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from 'next';
-import { Text, Flex, Spinner, HStack } from '@chakra-ui/react';
+import { Text, Flex, Spinner, HStack, Button, Spacer } from '@chakra-ui/react';
 import { useQueryBook } from '../src/hooks/useQueryBooks';
 import React from 'react';
 import { InputForm } from '../src/components/InputForm';
 import Typed from 'react-typed';
 import { BookGarageC } from '../src/components/BookGarageC';
-import { EditModal } from '../src/components/EditModal';
 
 const Home: NextPage = () => {
   const { status, data } = useQueryBook();
@@ -24,18 +23,21 @@ const Home: NextPage = () => {
   }
   return (
     <Flex direction="column" p="10" bg="aliceblue">
-      <Text
-        alignSelf="center"
-        bgGradient="linear(to-l, #b071eb,#ff4da6)"
-        bgClip="text"
-        fontSize="6xl"
-        fontWeight="extrabold"
-        mb="5"
-      >
-        <Typed strings={['OverView']} typeSpeed={100} />
-      </Text>
+      <HStack justifyContent="space-around">
+        <Text
+          alignSelf="center"
+          bgGradient="linear(to-l, #b071eb,#ff4da6)"
+          bgClip="text"
+          fontSize="6xl"
+          fontWeight="extrabold"
+          mb="5"
+          mr="10"
+        >
+          <Typed strings={['OverView']} typeSpeed={100} />
+        </Text>
+        <InputForm />
+      </HStack>
       <BookGarageC books={data} />
-      <InputForm />
     </Flex>
   );
 };
--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -2,18 +2,30 @@ import { useForm } from 'react-hook-form';
 import React from 'react';
 import {
   FormErrorMessage,
-  FormLabel,
   FormControl,
   Input,
   Button,
+  InputGroup,
+  InputLeftAddon,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
 } from '@chakra-ui/react';
 import { useMutationBook } from '../hooks/useMutationBooks';
 
 export const InputForm = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const {
     handleSubmit,
     register,
     reset,
+    setFocus,
     formState: { errors, isSubmitting },
   } = useForm();
   const { createBookMutation } = useMutationBook();
@@ -21,38 +33,67 @@ export const InputForm = () => {
   const onSubmit = (values) => {
     createBookMutation.mutate(values);
     reset();
+    setFocus('title');
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <FormControl isInvalid={errors.name}>
-        <FormLabel colorScheme="cyan" htmlFor="title">
-          BookTitle
-        </FormLabel>
-        <Input
-          id="title"
-          placeholder="Book Title"
-          {...register('title', {
-            required: 'This is required',
-          })}
-        />
-        <FormLabel colorScheme="cyan" htmlFor="model">
-          model
-        </FormLabel>
-        <Input
-          id="model"
-          placeholder="Book model"
-          {...register('model', {
-            required: 'This is required',
-          })}
-        />
-        <FormErrorMessage>
-          {errors.name && errors.name.message}
-        </FormErrorMessage>
-      </FormControl>
-      <Button mt={4} colorScheme="teal" isLoading={isSubmitting} type="submit">
-        Submit
+    <>
+      <Button onClick={onOpen} colorScheme="facebook">
+        Add record
       </Button>
-    </form>
+
+      <Modal isOpen={isOpen} onClose={onClose} size="xl">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <FormControl isInvalid={errors.name}>
+                <InputGroup alignItems="center" mb="3">
+                  <InputLeftAddon colorScheme="cyan" htmlFor="title">
+                    BookTitle
+                  </InputLeftAddon>
+                  <Input
+                    id="title"
+                    placeholder="Book Title"
+                    {...register('title', {
+                      required: 'This is required',
+                    })}
+                  />
+                </InputGroup>
+                <InputGroup>
+                  <InputLeftAddon colorScheme="cyan" htmlFor="model">
+                    model
+                  </InputLeftAddon>
+                  <Input
+                    id="model"
+                    placeholder="Book model"
+                    {...register('model', {
+                      required: 'This is required',
+                    })}
+                  />
+                </InputGroup>
+                <FormErrorMessage>
+                  {errors.name && errors.name.message}
+                </FormErrorMessage>
+              </FormControl>
+              <ModalFooter>
+                <Button
+                  colorScheme="teal"
+                  isLoading={isSubmitting}
+                  type="submit"
+                >
+                  Submit
+                </Button>
+                <Button colorScheme="blue" mr={3} onClick={onClose}>
+                  Close
+                </Button>
+              </ModalFooter>
+            </form>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
