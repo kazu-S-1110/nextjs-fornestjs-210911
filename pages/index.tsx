441aff8c9f3d4206e77f1cb990ac52a72debeffc
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-16 20:10:03 +0900

    react-tabsを使用。結構いいね♪

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,9 @@ import { useQueryBook } from '../src/hooks/useQueryBooks';
 import React from 'react';
 import { InputForm } from '../src/components/InputForm';
 import Typed from 'react-typed';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import { BookGarageC } from '../src/components/BookGarageC';
+import 'react-tabs/style/react-tabs.css';
 
 const Home: NextPage = () => {
   const { status, data } = useQueryBook();
@@ -23,21 +25,33 @@ const Home: NextPage = () => {
   }
   return (
     <Flex direction="column" p="10" bg="aliceblue">
-      <HStack justifyContent="space-around">
-        <Text
-          alignSelf="center"
-          bgGradient="linear(to-l, #b071eb,#ff4da6)"
-          bgClip="text"
-          fontSize="6xl"
-          fontWeight="extrabold"
-          mb="5"
-          mr="10"
-        >
-          <Typed strings={['OverView']} typeSpeed={100} />
-        </Text>
-        <InputForm />
-      </HStack>
-      <BookGarageC books={data} />
+      <Tabs>
+        <TabList>
+          <Tab>Garage</Tab>
+          <Tab>Setup</Tab>
+        </TabList>
+
+        <TabPanel>
+          <HStack justifyContent="space-around">
+            <Text
+              alignSelf="center"
+              bgGradient="linear(to-l, #b071eb,#ff4da6)"
+              bgClip="text"
+              fontSize="6xl"
+              fontWeight="extrabold"
+              mb="5"
+              mr="10"
+            >
+              <Typed strings={['Book In Garage']} typeSpeed={50} />
+            </Text>
+            <InputForm />
+          </HStack>
+          <BookGarageC books={data} />
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
     </Flex>
   );
 };
